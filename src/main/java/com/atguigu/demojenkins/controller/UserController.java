16cd01f4fef2e232e8a6406a40a5fe2af09529fa
lv-debug <lvgr888@163.com>
2021-05-25 18:05:58 +0800

    12312312321444443333

--- src/main/java/com/atguigu/demojenkins/controller/UserController.java
+++ src/main/java/com/atguigu/demojenkins/controller/UserController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String findAll() {
-        return "hello word!!";
+        return "hello word!!??";
     }
 }
